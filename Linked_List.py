835274a34ab6c3eaa7398c43341329e76515cfdb
Amirali <ashahinp@ucsc.edu>
2016-01-18 22:02:38 -0800

    My first commit

---
+++ Linked_List.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 23:04:10 2016
+
+@author: Amirali
+
+Purpuse: Linked List Implimentation
+"""
+
+
+class Node:
+    def __init__(self, data = None, nextNode = None):
+        self.data = data
+        self.nextNode = nextNode
+
+    def getNext(self):
+        return self.nextNode
+        
+    def setNext(self,nextNode):
+        self.nextNode = nextNode
+        
+    def getData(self):
+        return self.data
+        
+    def setData(self, data):
+        self.data = data
+        
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    # first we have to set the old head for the new node and then set the old head to new node
+    def insert(self,data):
+        temp = Node(data)
+        temp.setNext(self.head)
+        self.head = temp
+        
+    def isEmpty(self):
+        return self.head == None
+        
+        
+myFirst = LinkedList()
+myFirst.insert(3)
+print(myFirst.isEmpty())
+        

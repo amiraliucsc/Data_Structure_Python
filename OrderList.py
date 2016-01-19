2d2e6e08f9c123e6f3e077e868b321abbb508e6e
Amirali <ashahinp@ucsc.edu>
2016-01-19 01:17:12 -0800

    add()

--- OrderList.py
+++ OrderList.py
@@ -7,3 +7,56 @@ Order List
 """
 
 
+class Node:
+    def __init__(self, data = None, nextNode = None):
+        self.data = data
+        self.nextNode = nextNode
+    
+    def setNext(self,nextNode):
+        self.nextNode = nextNode
+        
+    def getNext(self):
+        return self.nextNode
+        
+    def setData(self,data):
+        self.data = data
+    
+    def getData(self):
+        return self.data
+
+
+
+class OrderList:
+    def __init__(self):
+        self.head = None
+
+    def add(self,item):
+        current = self.head
+        prev = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                prev = current
+                current = current.getNext()
+        temp = Node(item)
+        if prev == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            prev.setNext(temp)
+
+
+    def Traverse(self):
+        current = self.head
+        while current != None:
+            print(current.getData())
+            current = current.getNext()
+           
+link1 = OrderList()
+link1.add(3)
+link1.add(1)
+link1.add(5)
+link1.Traverse()

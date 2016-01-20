7e0fb24a05a1506e3e98c72a197a5d871c77341f
Amirali <ashahinp@ucsc.edu>
2016-01-19 17:50:15 -0800

    New Sort

--- Sort/insertionSort.py
+++ Sort/insertionSort.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import timeit
 from timeit import Timer
+import scipy
 
 def insertSort(A):
     for j in range(1,len(A)):
@@ -28,25 +29,30 @@ sizeArray = np.array([])
 timeArrayWorst = np.array([])
 timeArrayBest = np.array([])
 start_time = time.time()
-for i in range(1,10000):
+# Testing the worst case and base case 
+for i in range(1,200):
+    # Best case ( a sorted array)
     A = np.array(np.random.randint(0,50,i))
     A = np.sort(A)
     start = 0
     end = len(A)-1
     t = Timer(lambda: insertSort(A))
-    execTimeBest = t.timeit(number = 1)
+    execTimeBest = scipy.mean(t.repeat(repeat = 10,number = 1))
     timeArrayBest = np.append(timeArrayBest,execTimeBest)
-    # Worts case
-    
+    # Worts case  ( a decsending sorted array )
     A = np.array(np.random.randint(0,50,i))
     A = np.sort(A)
     A = A[::-1]
     start = 0
     end = len(A)-1
     t = Timer(lambda: insertSort(A))
-    execTimeWorst = t.timeit(number = 1)
+    execTimeWorst = scipy.mean(t.repeat(repeat = 10,number = 1))
     timeArrayWorst = np.append(timeArrayWorst,execTimeWorst)
     sizeArray = np.append(sizeArray, end)
+fig = plt.figure()
+fig.suptitle('Insertion Sort',fontsize = 14, fontweight = 'bold')    
+ax = fig.add_subplot(111)
+fig.subplots_adjust(top=0.85)
 plt.plot(sizeArray,timeArrayBest)
 plt.plot(sizeArray,timeArrayWorst)
 plt.show()        
---
+++ Sort/mergeSort.py
@@ -0,0 +1,82 @@
+"""
+Created on Thu Jan 12 11:25:32 2016
+
+@author: Amirali Shahinpour
+
+Name: Insertaion Sort
+"""
+
+import numpy as np
+import timeit
+from timeit import Timer
+import time
+import matplotlib.pyplot as plt
+import scipy
+def mergeSort(A,p,q):
+    if p < q:
+        mid = (p+q) // 2
+        mergeSort(A,p,mid)
+        mergeSort(A,mid+1,q)
+        merge(A,p,mid,q)
+
+def merge(A,s1,e1,e2):
+    k = s1
+    s2 = e1+1
+    L = np.copy(A[s1:e1+1])
+    R = np.copy(A[s2:e2+1])
+    i = 0
+    j = 0
+    while(i < len(L) and j < len(R)):
+        if (L[i] > R[j]):
+            A[k] = R[j]
+            j+=1
+        else:
+            A[k] = L[i]
+            i+=1
+        k+=1
+    # copy the rest of L or R into A    
+    while i < len(L):
+        A[k] = L[i]
+        i+=1
+        k+=1
+    while j < len(R):    
+        A[k] = R[j]
+        j+=1
+        k+=1
+
+
+start_time= time.time()
+sizeArray = np.array([])
+timeArrayBest = np.array([])
+timeArrayWorst = np.array([])
+# Testing the worst case and base case (to Proof there is no different)
+for i in range(1,200):
+    A = np.array(np.random.randint(0,50,i))
+    A = np.sort(A)
+    start = 0
+    end = len(A)-1
+    t = Timer(lambda: mergeSort(A,start,end))
+    execTimeBest = scipy.mean(t.repeat(repeat = 10,number = 1))
+    timeArrayBest = np.append(timeArrayBest,execTimeBest)    
+    
+    
+    #Worst
+    A = np.array(np.random.randint(0,50,i))
+    A = np.sort(A)
+    A = A[::-1]
+    start = 0
+    end = len(A)-1
+    t = Timer(lambda: mergeSort(A,start,end))
+    execTimeWorst = scipy.mean(t.repeat(repeat = 10,number = 1))
+    timeArrayWorst = np.append(timeArrayWorst,execTimeWorst)
+    sizeArray = np.append(sizeArray, end)
+    
+fig = plt.figure()
+fig.suptitle('Merge Sort', fontsize=14, fontweight='bold')
+ax = fig.add_subplot(111)
+fig.subplots_adjust(top=0.85)
+plt.plot(sizeArray,timeArrayBest)
+#plt.axis([0,1000,0,0.02])
+plt.plot(sizeArray,timeArrayWorst)
+plt.show()
+print("Runtime = ", (time.time() - start_time)," Sec")

42e47d0abfc7bade26608df17dd71d88de0c3d88
Amirali <ashahinp@ucsc.edu>
2016-01-19 19:08:34 -0800

    Recursion Exercises

--- Recursion/Palindromes.py
+++ Recursion/Palindromes.py
@@ -3,16 +3,27 @@ Created on Tue Jan 16 18:19:40 2016
 
 @author: Amirali Shahinpour
 
-Description: to check if a given string is Palindromes
+Description: to check if a given string is Palindromes or not
 """
 
 str = "madam"
+str2 = "Go hang a salami; I’m a lasagna hog."
+str3 = "kayak"
 
 def isPalindromes(str):
-    if len(str) == 1:
+    str = str.lower()
+    str = str.replace(".","")
+    str = str.replace(";","")
+    str = str.replace(" ","")
+    str = str.replace(",","")
+    str = str.replace("-","")
+    str = str.replace("’","")
+    str = str.replace("'","")
+    print(str)
+    if len(str) < 2:
         return True
     if str[0] == str[len(str)-1]: 
-        return checkPalid( str[1:len(str)-1] )
+        return isPalindromes( str[1:len(str)-1] )
     else: return False
         
-print(isPalindromes(str))
+print( isPalindromes(str2) )

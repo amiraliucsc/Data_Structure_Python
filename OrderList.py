0ff9ecf4ddc20517e2a48adc7ec4f558e8cf6860
Amirali <ashahinp@ucsc.edu>
2016-01-21 12:33:28 -0800

    pop() and search() has been add to OrderedList

--- OrderList.py
+++ OrderList.py
@@ -54,9 +54,43 @@ class OrderList:
         while current != None:
             print(current.getData())
             current = current.getNext()
+            
+    def search(self,data):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == data:
+                found = True
+            else:
+                if current.getData() > data:
+                    stop = True
+                else:
+                    current = current.getNext()
+        return found
            
+    def pop(self):
+        prev = None
+        stop = False
+        current = self.head
+        while current != None and not stop:
+            if current.getNext() != None:
+                prev = current
+                current = current.getNext()
+            else:
+                if prev != None:
+                    prev.setNext(None)
+                    stop = True
+                else:
+                    print("List is Empty!")
+                    stop = True
+            
 link1 = OrderList()
 link1.add(3)
 link1.add(1)
 link1.add(5)
+link1.Traverse()
+link1.pop()
+link1.pop()
+
 link1.Traverse()

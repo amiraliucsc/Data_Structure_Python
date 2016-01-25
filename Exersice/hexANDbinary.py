3fd15a6db848142f9b5d74d9cc436725d4a9f51e
Amirali <ashahinp@ucsc.edu>
2016-01-25 00:07:54 -0800

    Hex and Binary convertor

--- Exersice/hexANDbinary.py
+++ Exersice/hexANDbinary.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sun Jan 24 23:48:37 2016
 
-@author: Amirali
+@author: Amirali Shahinpour
+
+Convert binary to decimal and hex to decimal
 """
 
 def binaryToDecimal(binarySTR):
@@ -12,6 +13,11 @@ def binaryToDecimal(binarySTR):
             index = (len(binarySTR)-1) - i
             if binarySTR[i] == "1":
                 num += (2 ** index)
+            elif binarySTR[i] == "0":
+                pass
+            else:
+                print(binarySTR[i]+" is not a valid binary value, please enter either 0 or 1")
+                return 0
         return num
     else:
         return 0
@@ -23,25 +29,29 @@ def hexToDecimal(hexSTR):
         val = 0
         for i in range(len(hexSTR)-1,-1,-1):
             index = (len(hexSTR)-1 ) - i
-            if hexSTR[i] == "A" or hexSTR[i] == "a":
-                val = 10
-            elif hexSTR[i] == "B" or hexSTR[i] == "b":
-                val = 11
-            elif hexSTR[i] == "C" or hexSTR[i] == "c":
-                val = 12
-            elif hexSTR[i] == "D" or hexSTR[i] == "d":
-                val = 13
-            elif hexSTR[i] == "E" or hexSTR[i] == "e":
-                val = 14
-            elif hexSTR[i] == "F" or hexSTR[i] == "f":
-                val = 15
-            elif int(hexSTR[i]) < 10:
-                val = int(hexSTR[i])    
-            num+= val * (16 ** index)
+            try:
+                if hexSTR[i] == "A" or hexSTR[i] == "a":
+                    val = 10
+                elif hexSTR[i] == "B" or hexSTR[i] == "b":
+                    val = 11
+                elif hexSTR[i] == "C" or hexSTR[i] == "c":
+                    val = 12
+                elif hexSTR[i] == "D" or hexSTR[i] == "d":
+                    val = 13
+                elif hexSTR[i] == "E" or hexSTR[i] == "e":
+                    val = 14
+                elif hexSTR[i] == "F" or hexSTR[i] == "f":
+                    val = 15
+                elif int(hexSTR[i]) < 10:
+                    val = int(hexSTR[i])    
+                num+= val * (16 ** index)
+            except ValueError:
+                print(hexSTR[i]+" is not a valid hexadecimal value please enter 0-9 and A-F.")
+                return 0
         return num
     else:
         return 0
     
     
-#print(binaryToDecimal('1000000000001'))
-print(hexToDecimal('FF'))
+print(binaryToDecimal('110101011111111111111111111111111111111111111'))
+print(hexToDecimal('FDCfcaa234F0FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA1'))

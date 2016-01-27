e8e8f544c9f5670d6aa43ef3f37d5c42c1728179
Amirali <ashahinp@ucsc.edu>
2016-01-27 00:45:34 -0800

    Heap

---
+++ Heap.py
@@ -0,0 +1,15 @@
+"""
+Created on Wed Jan 27 00:27:35 2016
+
+@author: Amirali
+
+Implementation of Heap
+"""
+
+class Heap:
+    def __init__(self, key):
+        self.root = key
+        self.left = None
+        self.right = None
+        
+

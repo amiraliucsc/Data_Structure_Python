f0b3c7d4a242d5a6458a93507c48588f03d55cdb
Amirali <ashahinp@ucsc.edu>
2016-02-03 00:20:17 -0800

    Modification on Heap data structre

--- Heap.py
+++ Heap.py
@@ -3,29 +3,34 @@ Created on Wed Jan 27 00:27:35 2016
 
 @author: Amirali
 
-Implementation of Heap
+Implementation of Min Heap
 """
 
 class MinHeap:
-    def __init__(self, key):
-        self.root = key
-        self.left = None
-        self.right = None
+    def __init__(self):
+        self.current_size = 0
+        self.heap_list = [0]
         
     def insert(self,key):
-        if self.root < key:
-            if self.left == None:
-                self.left = MinHeap(key)
-            elif self.right == None:
-                self.right == MinHeap(key)
-            else:
-                while(self.left != None):
-                    self.left.insert(key)
+        if self.current_size == 0:
+            self.current_size == 1
+            self.heap_list.append( key )
+        else:
+            self.current_size+=1
+            self.heap_list.append( key )
+            self.reHeapify(self.current_size)
     
-    def remove(self,key):
+    def reHeapify(self,current):
+        if current > 0 and current != 1:                
+            parent = current // 2
+            if self.heap_list[parent] > self.heap_list[current]:
+                temp = self.heap_list[current]
+                self.heap_list[current] = self.heap_list[parent]
+                self.heap_list[parent] = temp
+                self.reHeapify(parent)
                     
                     
-myHeap = MinHeap(3)
+myHeap = MinHeap()
 myHeap.insert(4)
                 
         
